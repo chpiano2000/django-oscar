1b1d0ed5ddff5c086b0cff84a009c00f68e4df6f
vochidat0 <dat.usth2000@gmail.com>
2023-08-27 00:20:46 +0700

    change: remove redirect to catalog homepage

--- src/oscar/apps/catalogue/views.py
+++ src/oscar/apps/catalogue/views.py
@@ -140,7 +140,6 @@ class CatalogueView(TemplateView):
         except InvalidPage:
             # Redirect to page one.
             messages.error(request, _("The given page number was invalid."))
-            return redirect("catalogue:index")
         return response
 
     def get_search_handler(self, *args, **kwargs):
